e7d4c627b2d3c0ecb7c991609824bffc714f4b15
Rockyfelle <rockyfelle@live.se>
2022-04-12 12:50:21 +0200

    Fix Merge Error

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import LoginPage from '../pages/LoginPage';
+import { Grid, Segment } from 'semantic-ui-react';
 
 
 function App() {
 	return (
-		<LoginPage />
+		<div className="m-0">
+			<Grid className="m-0">
+				<Grid.Row columns={16} className="p-0">
+					<Grid.Column width={2} className="p-0">
+						<Segment className="h-[100vh]">
+							Sidebar 1
+						</Segment>
+					</Grid.Column>
+					<Grid.Column width={2} className="p-0">
+						<Segment className="h-[100vh]">
+							Sidebar 2
+						</Segment>
+					</Grid.Column>
+					<Grid.Column width={10} className="p-0">
+						<Segment className="h-[100vh]">
+							<LoginPage />
+						</Segment>
+					</Grid.Column>
+					<Grid.Column width={2} className="p-0">
+						<Segment className="h-[100vh]">
+							Sidebar 3
+						</Segment>
+					</Grid.Column>
+				</Grid.Row>
+			</Grid>
+		</div>
+
 	);
 }
 
